6882d2ea5fce1db84ad1339cdd0c842987cba5d2
WoliBlue <trabajojosearenasvenegas@gmail.com>
2024-05-09 11:19:02 +0200

    terminado (creo)

--- Practica090524/src/tests/SuitePruebasPractica.java
+++ Practica090524/src/tests/SuitePruebasPractica.java
@@ -4,7 +4,14 @@ import org.junit.platform.runner.JUnitPlatform;
 import org.junit.platform.suite.api.SelectClasses;
 import org.junit.runner.RunWith;
 
+
+@RunWith(JUnitPlatform.class)
+@SelectClasses({
+    MediaNotasTest.class,
+    MedianaNotasTest.class,
+    MaximaNotaTest.class,
+    MinimaNotaTest.class
+})
 public class SuitePruebasPractica {
-
-
+  
 }
