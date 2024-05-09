639b6ef021a46f59a6831269a392c8e75033679c
WoliBlue <trabajojosearenasvenegas@gmail.com>
2024-05-09 10:25:03 +0200

    float y throw

--- Practica090524/src/util/MisArrays.java
+++ Practica090524/src/util/MisArrays.java
@@ -5,7 +5,8 @@ import java.util.Arrays;
 public class MisArrays {
 //Jose Arenas Venegas
 	
-    public static int mediaNotas(int[] notas) {
+	public static float mediaNotas(int[] notas) {
+		validarNota(notas);
     	int media = 0;
         int sum = 0;
         for (int nota : notas) {
@@ -15,12 +16,13 @@ public class MisArrays {
         return media;
     }
     
-    public static int medianaNotas(int[] notas) {
+    public static float medianaNotas(int[] notas) {
+    	validarNota(notas);
         Arrays.sort(notas);
-        int mediana;
+        float mediana;
         int middle = notas.length / 2;
         if (notas.length % 2 == 0) {
-            mediana = (notas[middle] + notas[middle - 1]) / 2;
+            mediana = (notas[middle] + notas[middle - 1]) / 2.0f;
         } else {
             mediana = notas[middle];
         }
@@ -28,6 +30,7 @@ public class MisArrays {
     }
 
     public static int maximaNota(int[] notas) {
+    	validarNota(notas);
         int max = 0;
         for (int nota : notas) {
             if (nota > max) {
@@ -36,6 +39,23 @@ public class MisArrays {
         }
         return max;
     }
-
+    public static int minimaNota(int[] notas) {
+    	validarNota(notas);
+        int min = 10;
+        for (int nota : notas) {
+            if (nota < min) {
+                min = nota;
+            }
+        }
+        return min;
+    }
     
+    private static void validarNota(int[] notas) {
+        for (int nota : notas) {
+            if (nota < 0 || nota > 10) {
+                throw new IllegalArgumentException("Debe ser entre 0 y 10");
+            }
+        }
+    }
 }
+    
