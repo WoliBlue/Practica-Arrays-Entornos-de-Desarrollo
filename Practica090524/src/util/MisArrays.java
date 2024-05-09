8298fb2bba72be1bac780646d0d4ab94b689fa9f
WoliBlue <trabajojosearenasvenegas@gmail.com>
2024-05-09 10:40:59 +0200

    y ahora toca los test

--- Practica090524/src/util/MisArrays.java
+++ Practica090524/src/util/MisArrays.java
@@ -4,9 +4,14 @@ import java.util.Arrays;
 
 public class MisArrays {
 //Jose Arenas Venegas
-	
+
+    /**
+     * @param Array de notas entre 0 y 10.
+     * @return La media de las notas.
+     * @throws IllegalArgumentException no esta entre 0 y 10.
+     */
 	public static float mediaNotas(int[] notas) {
-		validarNota(notas);
+		validaNota(notas);
     	int media = 0;
         int sum = 0;
         for (int nota : notas) {
@@ -16,8 +21,13 @@ public class MisArrays {
         return media;
     }
     
+    /**
+     * @param Array de notas entre 0 y 10.
+     * @return La mediana de las notas.
+     * @throws IllegalArgumentException no esta entre 0 y 10.
+     */
     public static float medianaNotas(int[] notas) {
-    	validarNota(notas);
+    	validaNota(notas);
         Arrays.sort(notas);
         float mediana;
         int middle = notas.length / 2;
@@ -28,9 +38,13 @@ public class MisArrays {
         }
         return mediana;
     }
-
+    /**
+     * @param Array de notas entre 0 y 10.
+     * @return La maxima de las notas xd.
+     * @throws IllegalArgumentException no esta entre 0 y 10.
+     */
     public static int maximaNota(int[] notas) {
-    	validarNota(notas);
+    	validaNota(notas);
         int max = 0;
         for (int nota : notas) {
             if (nota > max) {
@@ -39,8 +53,14 @@ public class MisArrays {
         }
         return max;
     }
+    
+    /**
+     * @param Array de notas entre 0 y 10.
+     * @return La minima de las notas.
+     * @throws IllegalArgumentException no esta entre 0 y 10.
+     */
     public static int minimaNota(int[] notas) {
-    	validarNota(notas);
+    	validaNota(notas);
         int min = 10;
         for (int nota : notas) {
             if (nota < min) {
@@ -49,11 +69,13 @@ public class MisArrays {
         }
         return min;
     }
-    
-    private static void validarNota(int[] notas) {
+    /**
+     * @param Valida array notas.
+     */
+    private static void validaNota(int[] notas) {
         for (int nota : notas) {
             if (nota < 0 || nota > 10) {
-                throw new IllegalArgumentException("Debe ser entre 0 y 10");
+                throw new IllegalArgumentException("debe ser entre 0 y 10 lol");
             }
         }
     }
